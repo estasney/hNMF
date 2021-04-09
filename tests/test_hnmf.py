6d3f1154603c8e18c528b00ab32c56759a7d2314
estasney <estasney@users.noreply.github.com>
2021-04-08 22:40:00 -0400

    use pytest

--- tests/test_hnmf.py
+++ tests/test_hnmf.py
@@ -1,3 +1,5 @@
+import pytest
+
 from hNMF import model as testmodel
 from sklearn.datasets import make_sparse_spd_matrix
 from scipy.stats import beta
@@ -5,30 +7,24 @@ import unittest
 import numpy as np
 
 
-class MyTestCase(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.n_samples = np.random.randint(100, 30000)
-        self.features_coeff = np.random.choice(np.linspace(0.1, 3))
-        self.n_features = int(self.n_samples * self.features_coeff)
-        self.alpha = beta(10, 1).rvs()
-        self.X = np.abs(make_sparse_spd_matrix(dim=self.n_samples, alpha=self.alpha, norm_diag=False,
-                                               smallest_coef=0.1, largest_coef=0.7))
-        self.k = np.random.randint(5, 50)
-
-    def test_hnmf_inits(self):
-        try:
-            model = testmodel.HierarchicalNMF(k=self.k)
-        except:
-            raise AssertionError
-
-    def test_hnmf_runs(self):
-        try:
-            model = testmodel.HierarchicalNMF(k=self.k)
-            model.fit(self.X)
-        except:
-            raise AssertionError
+@pytest.fixture(scope='module')
+def sample_data():
+    n_samples = np.random.randint(100, 30000)
+    features_coeff = np.random.choice(np.linspace(0.1, 3))
+    n_features = int(n_samples * features_coeff)
+    alpha = beta(10, 1).rvs()
+    X = np.abs(make_sparse_spd_matrix(dim=n_samples, alpha=alpha, norm_diag=False,
+                                      smallest_coef=0.1, largest_coef=0.7))
+    k = np.random.randint(5, 50)
+    return X, k
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_hnmf_inits(sample_data):
+    X, k = sample_data
+    model = testmodel.HierarchicalNMF(k)
+
+
+def test_hnmf_runs(sample_data):
+    X, k = sample_data
+    model = testmodel.HierarchicalNMF(k)
+    model.fit(X)
